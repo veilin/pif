b5020268096324f073cef4778f4da4202ca2c17d
rudolfl <rudolfl@xpedia.co.za>
2018-10-11 14:05:02 +0200

    My changes

--- src/app/contract/contract.component.ts
+++ src/app/contract/contract.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '@env/environment';
+import { IContract } from '@app/icontract';
 
 @Component({
   selector: 'app-contract',
@@ -9,9 +10,29 @@ import { environment } from '@env/environment';
 export class ContractComponent implements OnInit {
   version: string = environment.version;
 
+  contracts: IContract[] = [];
+
   constructor() {
   }
 
   ngOnInit() {
+
+    let contract: IContract;
+    contract = {
+      id: 0,
+      value: 10347.21,
+      description: 'This ia a dslkfhsdlkfhsdklflksdhfsdhflk kdsflk sdhflkh sdlkhflsdkhfk sldhfkhs khdsfkh sdlkhfkl ' +
+        'sdhflhsdlkfh skldhfkl hsdlkfh sldkhf lsdkhfl khsd' +
+        'sdlkfjsd kjflksdj lkfjsdkljflk jsdkfj ksldjfskl dflsdhflksdufksdhfksdlkhf lkdshf d' +
+        'dsklf jsdlkfj lksdjflkjs dlkfjs dlkjflksd jflkjs dlkjfsldk jflksdjf klsdjf lksdjf kljsdklfj ksldjfkl sdlkfj',
+      name: 'Contract name'
+    };
+
+    this.contracts.push(contract);
+
+  }
+
+  trackContractById(index: number, contract: IContract) {
+    return contract.id;
   }
 }
--- src/app/icontract.ts
+++ src/app/icontract.ts
@@ -1,7 +1,8 @@
 export interface IContract {
 
-  value: 0.00;
-  name: 'Contract name';
-  description: 'Description';
+  id: number;
+  value: number;
+  name: string;
+  description: string;
 
 }

2f566e881a5e4b4668aa1fa277497b255cff5526
Johan Steenkamp <johans@spsi.co.za>
2018-10-12 12:58:02 +0200

    modified:   app/home/home.component.html 	modified:   app/home/home.component.scss 	modified:   app/home/home.component.ts 	modified:   app/home/home.module.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,8 @@ import { LocalStorage } from 'ngx-store';
 export class HomeComponent implements OnInit {
   quote: string;
   isLoading: boolean;
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 
   @LocalStorage()
   num: number;
